12dbbda65a612b0ef87ed53715ce3dbc5023267c
makotokoshiba <figaro.5483@gmail.com>
2025-06-02 16:02:10 +0900

    Vue Router導入

---
+++ resources/js/Layouts/AppLayout.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <nav class="p-4 bg-gray-100">
+            <router-link to="/" class="mr-4">Home</router-link>
+            <router-link to="/todo">Todo</router-link>
+        </nav>
+
+        <main class="p-4">
+            <slot />
+        </main>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppLayout',
+};
+</script>
---
+++ resources/js/Pages/Todo.vue
@@ -0,0 +1,13 @@
+<template>
+    <AppLayout>
+        <h1 class="text-2xl font-bold">todoページ</h1>
+    </AppLayout>
+</template>
+
+<script>
+import AppLayout from '../Layouts/AppLayout.vue';
+
+export default {
+    components: { AppLayout },
+};
+</script>
