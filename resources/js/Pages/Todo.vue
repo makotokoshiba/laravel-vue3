6012f200792959a46363a41fcaefdc1b5b343890
makoto_koshiba <figaro.5483@gmail.com>
2025-06-12 18:38:21 +0900

    veevalidateインストール

--- resources/js/Pages/Todo.vue
+++ resources/js/Pages/Todo.vue
@@ -1,18 +1,23 @@
 <template>
-    <h1 class="text-2xl font-bold mb-4">Todoリスト</h1>
-  
-    <div class="mb-4">
-        <input
-          v-model="newTodo"
-          @keyup.enter="addTodo"
+  <main class="p-8">
+    <h1 class="text-2xl font-bold mb-4">Todo Form</h1>
+
+    <Form @submit="submitHandler">
+      <div class="mb-4">
+        <Field
+          name="task"
+          rules="required"
+          v-model="task"
           placeholder="新しいタスクを入力"
-          class="border p-2 rounded w-64"
+          class="border p-2 rounded"
         />
-        <button @click="addTodo" class="ml-2 bg-blue-500 text-white px-4 py-2 rounded">
+        <ErrorMessage name="task" class="text-red-500 text-sm mt-1" />
+        <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded">
           追加
         </button>
-    </div>
-  
+      </div>
+    </Form>
+
     <ul>
         <li
           v-for="(todo, index) in todos"
@@ -23,24 +28,40 @@
           <button @click="removeTodo(index)" class="text-red-500">削除</button>
         </li>
       </ul>
-  </template>
-  
-  
+  </main>
+</template>
+
 <script setup lang="ts">
 import { ref } from 'vue'
+import { Form, Field, ErrorMessage, useForm } from 'vee-validate'
 
-const newTodo = ref<string>('')
 const todos = ref<string[]>([])
+const task = ref<string>('')
 
-const addTodo = () => {
-    if (newTodo.value.trim()) {
-        todos.value.push(newTodo.value.trim())
-        newTodo.value = ''
-    }
+const { handleSubmit, resetForm } = useForm({
+  initialValues: { task: ''},
+})
+
+const onSubmit = () => {
+  const trimmed = task.value.trim()
+  if (trimmed) {
+    todos.value.push(trimmed)
+    task.value = null
+    
+    // フォームリセット リセット後のエラーを回避
+    resetForm()
+  }
 }
+const submitHandler = handleSubmit(onSubmit);
 
 const removeTodo = (index: number) => {
     todos.value.splice(index, 1)
 }
+
 </script>
-  
+
+<style>
+body {
+  font-family: sans-serif;
+}
+</style>
